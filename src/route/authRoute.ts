5d9cb879cbf504687e319c598a6c715eda23a6ee
duquenascimento <dukedavi84@gmail.com>
2024-12-10 17:37:01 -0300

    ajuste

--- src/route/authRoute.ts
+++ src/route/authRoute.ts
@@ -1,20 +1,14 @@
 import { type FastifyInstance } from 'fastify'
-import {
-  firstStepSignUp, type IFirstStepSignUpRequest,
-  checkLogin, type ICheckLogin,
-  type IFirstStepSignUp
-} from '../service/authService'
-import { signInFirstStepUser } from '../repository/authRepository'
+import { firstStepSignUp, type IFirstStepSignUpRequest, checkLogin, type ICheckLogin, signIn, PwRecoveryCreateService, PwRecoveryCheckService, PwChange } from '../service/authService'
 
 export const authRoute = async (server: FastifyInstance): Promise<void> => {
   server.post('/auth/signup', async (req, res): Promise<any> => {
-    console.log('>>>>>>>>>>>>', req.body)
     try {
       const result = await firstStepSignUp(req.body as IFirstStepSignUpRequest)
       if (result == null) throw Error('first step of signUp failed')
       return await res.status(201).send({
         status: 201,
-        token: result
+        data: result
       })
     } catch (err) {
       const message = (err as Error).message
@@ -33,18 +27,14 @@ export const authRoute = async (server: FastifyInstance): Promise<void> => {
   })
 
   server.post('/auth/signin', async (req, res): Promise<any> => {
-    console.log('>>>>>>>>>>>>', req.body)
     try {
-      // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
-      const result = await signInFirstStepUser(req.body as IFirstStepSignUp)
-      console.log('>>result>>>>>>>>', result)
-      if (result == null) throw Error('first step of signUp failed')
+      const result = await signIn(req.body as IFirstStepSignUpRequest)
+      if (result == null) throw Error('signIn failed')
       return await res.status(201).send({
         status: 201,
-        token: result
+        data: result
       })
     } catch (err) {
-      console.log('>>>>>>>>>>>>', err)
       const message = (err as Error).message
       if (message === process.env.INTERNAL_ERROR_MSG) {
         await res.status(500).send({
@@ -53,7 +43,7 @@ export const authRoute = async (server: FastifyInstance): Promise<void> => {
         })
       } else {
         await res.status(409).send({
-          status: 409,
+          status: 401,
           msg: message
         })
       }
@@ -66,7 +56,73 @@ export const authRoute = async (server: FastifyInstance): Promise<void> => {
       if (result == null) throw Error('not logged')
       return await res.status(200).send({
         status: 200,
-        token: result
+        data: result
+      })
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        await res.status(500).send({
+          status: 500,
+          msg: message
+        })
+      } else {
+        await res.status(401).send({
+          status: 401,
+          msg: message
+        })
+      }
+    }
+  })
+
+  server.post('/auth/recovery', async (req, res): Promise<any> => {
+    try {
+      await PwRecoveryCreateService(req.body as { email: string })
+      return await res.status(200).send({
+        status: 200
+      })
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        await res.status(500).send({
+          status: 500,
+          msg: message
+        })
+      } else {
+        await res.status(401).send({
+          status: 401,
+          msg: message
+        })
+      }
+    }
+  })
+
+  server.post('/auth/recoveryCheck', async (req, res): Promise<any> => {
+    try {
+      await PwRecoveryCheckService(req.body as { email: string, codeSent: string })
+      return await res.status(200).send({
+        status: 200
+      })
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        await res.status(500).send({
+          status: 500,
+          msg: message
+        })
+      } else {
+        await res.status(401).send({
+          status: 401,
+          msg: message
+        })
+      }
+    }
+  })
+
+  server.post('/auth/pwChange', async (req, res): Promise<any> => {
+    try {
+      await PwChange(req.body as { email: string, codeSent: string, newPW: string })
+      return await res.status(200).send({
+        status: 200
       })
     } catch (err) {
       const message = (err as Error).message
