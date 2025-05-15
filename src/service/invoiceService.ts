a7a4447772e638bf6c27d7e1805869ce7c2de13f
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-05-15 11:30:59 -0300

    NF sendo salva no bucket S3

--- src/service/invoiceService.ts
+++ src/service/invoiceService.ts
@@ -1,15 +1,14 @@
 import { type order_invoice } from '@prisma/client'
 import { findInvoice, upsertInvoice } from '../repository/invoiceRepository'
 import { saveFile } from '../utils/utils'
+import { uploadPdfFileToS3 } from '../utils/uploadToS3Utils'
 
-export const upsert = async ({
-  filePath,
-  orderId
-}: Pick<order_invoice, 'filePath' | 'orderId'>): Promise<void> => {
+export const upsert = async ({ filePath, orderId }: Pick<order_invoice, 'filePath' | 'orderId'>): Promise<void> => {
   // Processa os arquivos
   const files = await Promise.all(
     filePath.map(async (file, i) => {
-      const savedFileUrl = await saveFile(file, `invoice-${i}-${orderId}.pdf`)
+      // const savedFileUrl = await saveFile(file, `invoice-${i}-${orderId}.pdf`)
+      const savedFileUrl = await uploadPdfFileToS3(file, `NF/invoice-${i}-${orderId}.pdf`)
       if (!savedFileUrl) {
         console.error(`Falha ao salvar o arquivo ${file}`)
         return null
@@ -20,6 +19,8 @@ export const upsert = async ({
 
   // Filtra os arquivos salvos com sucesso
   const validFiles = files.filter((file): file is string => file !== null)
+
+  console.log(validFiles)
 
   // Chama o upsertInvoice para criar ou atualizar o registro
   if (validFiles.length > 0) {
