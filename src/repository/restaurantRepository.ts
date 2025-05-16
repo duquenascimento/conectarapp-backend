b33afab863f318a61b2d72661f3c2def4da22463
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-05-16 18:34:34 -0300

    fix: adicionado rota para atualizacao do comercial block

--- src/repository/restaurantRepository.ts
+++ src/repository/restaurantRepository.ts
@@ -203,6 +203,24 @@ export const updateAddress = async (addressId: string, data: any): Promise<any> 
   }
 }
 
+export const updateRegistrationReleasedNewAppRepository = async (externalId: string, registrationReleasedNewApp: boolean): Promise<void> => {
+  try {
+    await prisma.restaurant.updateMany({
+      where: {
+        externalId
+      },
+      data: {
+        registrationReleasedNewApp
+      }
+    })
+  } catch (err: any) {
+    await prisma.$disconnect()
+    await logRegister(err)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
 export const updateComercialBlockRepository = async (restId: string, value: boolean): Promise<void> => {
   try {
     await prisma.restaurant.updateMany({
@@ -216,24 +234,6 @@ export const updateComercialBlockRepository = async (restId: string, value: bool
   } catch (err: any) {
     await prisma.$disconnect()
     await logRegister(err)
-  } finally {
-    await prisma.$disconnect()
-  }
-}
-
-export const updateRegistrationReleasedNewAppRepository = async (restId: string, value: boolean): Promise<void> => {
-  try {
-    await prisma.restaurant.updateMany({
-      where: {
-        externalId: restId
-      },
-      data: {
-        registrationReleasedNewApp: value
-      }
-    })
-  } catch (err: any) {
-    await logRegister(err)
-    throw new Error(process.env.INTERNAL_ERROR_MSG)
   } finally {
     await prisma.$disconnect()
   }
@@ -272,10 +272,7 @@ export const updateAllowCloseSupplierAndMinimumOrderRepository = async (req: Pic
   }
 }
 
-export const updateRestaurantRepository = async (
-  externalId: string,
-  restaurantData: Partial<restaurant>
-): Promise<void> => {
+export const updateRestaurantRepository = async (externalId: string, restaurantData: Partial<restaurant>): Promise<void> => {
   try {
     await prisma.restaurant.updateMany({
       data: restaurantData,
@@ -290,10 +287,7 @@ export const updateRestaurantRepository = async (
   }
 }
 
-export const updateAddressByExternalIdRepository = async (
-  externalId: string,
-  addressData: Partial<address>
-): Promise<void> => {
+export const updateAddressByExternalIdRepository = async (externalId: string, addressData: Partial<address>): Promise<void> => {
   try {
     const restaurant = await prisma.restaurant.findFirst({
       where: {
@@ -322,10 +316,7 @@ export const updateAddressByExternalIdRepository = async (
   }
 }
 
-export const patchRestaurantRepository = async (
-  externalId: string,
-  restaurantData: Partial<restaurant>
-): Promise<void> => {
+export const patchRestaurantRepository = async (externalId: string, restaurantData: Partial<restaurant>): Promise<void> => {
   try {
     await prisma.restaurant.updateMany({
       data: restaurantData,
