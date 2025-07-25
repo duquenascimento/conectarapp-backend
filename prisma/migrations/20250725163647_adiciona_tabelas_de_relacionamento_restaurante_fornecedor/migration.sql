c6f26b07cd962604f79f160156df0d8b2783f633
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-07-25 15:08:18 -0300

    migrations supplier restaurant

---
+++ prisma/migrations/20250725163647_adiciona_tabelas_de_relacionamento_restaurante_fornecedor/migration.sql
@@ -0,0 +1,33 @@
+ALTER TABLE "restaurant" ADD COLUMN "asaasCustomerId" TEXT;
+
+-- CreateTable
+CREATE TABLE "supplier" (
+    "id" TEXT NOT NULL,
+    "externalId" VARCHAR(50) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(50),
+    "asaasApiToken" VARCHAR(255) NOT NULL,
+    "asaasWalletId" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+    CONSTRAINT "supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "restaurant_supplier" (
+    "id" UUID NOT NULL,
+    "restaurantId" UUID NOT NULL,
+    "supplierId" UUID NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL,
+    CONSTRAINT "restaurant_supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "restaurant_supplier"
+ADD CONSTRAINT "restaurant_supplier_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "restaurant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "restaurant_supplier"
+ADD CONSTRAINT "restaurant_supplier_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "supplier" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
