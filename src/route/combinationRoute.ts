a41e407bd4e730a0b899d6f063e18e483e19a809
Luiz Silva <luiz.qos@gmail.com>
2025-08-11 13:33:27 -0300

    Criado rota de atualização de combinação

---
+++ src/route/combinationRoute.ts
@@ -0,0 +1,71 @@
+import { type FastifyInstance } from 'fastify'
+import { getCombination, postCombination, putCombination } from '../service/combinationService'
+import { combinacaoSchema } from '../validators/combinationValidator'
+import { type CombinacaoInput } from '../types/combinationType'
+
+export const combinationRoute = async (server: FastifyInstance): Promise<void> => {
+  server.get('/getCombination/:restauranId', async (req, res) => {
+    try {
+      const { restaurantId } = req.params as { restaurantId: string }
+
+      const result = await getCombination(restaurantId)
+      await res.status(200).send({
+        status: 200,
+        return: result
+      })
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        return await res.status(500).send({ status: 500, msg: message })
+      }
+      return await res.status(400).send({ status: 400, msg: message })
+    }
+  })
+
+  server.post('/setCombination', async (req, res) => {
+    try {
+      const { value, error } = combinacaoSchema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: true
+      })
+
+      if (error) {
+        return await res.status(400).send({ status: 400, msg: error.details.map((d) => d.message).join(', ') })
+      }
+
+      const body: CombinacaoInput = value
+
+      await postCombination(body)
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        return await res.status(500).send({ status: 500, msg: message })
+      }
+      return await res.status(400).send({ status: 400, msg: message })
+    }
+  })
+
+  server.put('/combination/:id/update', async (req, res) => {
+    try {
+      const { id } = req.params as { id: string }
+      const { value, error } = combinacaoSchema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: true
+      })
+
+      if (error) {
+        return await res.status(400).send({ status: 400, msg: error.details.map((d) => d.message).join(', ') })
+      }
+
+      const body: CombinacaoInput = value
+
+      await putCombination(id, body)
+    } catch (err) {
+      const message = (err as Error).message
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        return await res.status(500).send({ status: 500, msg: message })
+      }
+      return await res.status(400).send({ status: 400, msg: message })
+    }
+  })
+}
---
+++ src/service/combinationService.ts
@@ -0,0 +1,17 @@
+import { ApiRepository } from '../repository/apiRepository'
+import { type CombinacaoInput } from '../types/combinationType'
+
+const apiDbConectar = new ApiRepository(process.env.API_DB_CONECTAR ?? '')
+
+export const getCombination = async (restaurant_id: string): Promise<any> => {
+  const combinationRoute = await apiDbConectar.callApi(`/system/combinacao/${restaurant_id}`, 'GET')
+  return combinationRoute.data
+}
+
+export const postCombination = async (body: CombinacaoInput) => {
+  return await apiDbConectar.callApi('/system/combinacao', 'POST', JSON.stringify(body))
+}
+
+export const putCombination = async (combinationId: string, body: CombinacaoInput) => {
+  return await apiDbConectar.callApi(`/system/combinacao/${combinationId}`, 'PUT', JSON.stringify(body))
+}
