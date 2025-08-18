4403b294c25e9cc47b5c5731fac8cb4a5b35fb2e
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-08-18 09:22:09 -0300

    feat: removendo limitação de 5 fornecedores para calculo de cotacao

--- src/utils/premiumCestaProdutos.ts
+++ src/utils/premiumCestaProdutos.ts
@@ -1,18 +1,36 @@
 import axios from 'axios'
 import { ApiRepository } from '../repository/apiRepository'
 import { type ICartResponse } from '../service/cartService'
-import { type FornecedorMotor, type ProdutoCesta, type CombinacaoAPI, type CombinationResponse, type MotorCombinacaoRequest, type MotorCombinacaoResponse, type MotorCombinacaoWithSupplierNames, type PreferenciaClasse, type PreferenciaProduto } from '../types/quotationTypes'
+import {
+  type FornecedorMotor,
+  type ProdutoCesta,
+  type CombinacaoAPI,
+  type CombinationResponse,
+  type MotorCombinacaoRequest,
+  type MotorCombinacaoResponse,
+  type MotorCombinacaoWithSupplierNames,
+  type PreferenciaClasse,
+  type PreferenciaProduto
+} from '../types/quotationTypes'
 import { HttpException } from '../errors/httpException'
 import { preferencesResolver } from './premium-preferences.utils'
-import { getSuppliersFromPriceList, addSupplierNames } from './premium-suppliers.utils'
+import {
+  getSuppliersFromPriceList,
+  addSupplierNames
+} from './premium-suppliers.utils'
 
 const apiDbConectar = new ApiRepository(process.env.API_DB_CONECTAR ?? '')
 
-export async function cestaProdutos(cart: ICartResponse[]): Promise<ProdutoCesta[]> {
+export async function cestaProdutos(
+  cart: ICartResponse[]
+): Promise<ProdutoCesta[]> {
   const cesta: ProdutoCesta[] = []
 
   for (const item of cart) {
-    const produto = await apiDbConectar.callApi(`/system/produtos/${item.productId}`, 'GET')
+    const produto = await apiDbConectar.callApi(
+      `/system/produtos/${item.productId}`,
+      'GET'
+    )
 
     if (!produto) {
       throw new Error('Erro ao buscar produtos na base de dados')
@@ -28,8 +46,15 @@ export async function cestaProdutos(cart: ICartResponse[]): Promise<ProdutoCesta
   return cesta
 }
 
-export async function solveCombinations(prices: any[], products: ProdutoCesta[], restaurant: any): Promise<CombinationResponse[] | undefined> {
-  const combinationsResult = await apiDbConectar.callApi(`/system/combinacao/${restaurant.id}`, 'GET')
+export async function solveCombinations(
+  prices: any[],
+  products: ProdutoCesta[],
+  restaurant: any
+): Promise<CombinationResponse[] | undefined> {
+  const combinationsResult = await apiDbConectar.callApi(
+    `/system/combinacao/${restaurant.id}`,
+    'GET'
+  )
   const combinations = combinationsResult.data as CombinacaoAPI[]
 
   const reqSuppliers = await getSuppliersFromPriceList(prices, products)
@@ -44,18 +69,23 @@ export async function solveCombinations(prices: any[], products: ProdutoCesta[],
   for (const combination of combinations) {
     const favoriteCategories: PreferenciaClasse[] = []
     const favoriteProducts: PreferenciaProduto[] = []
-    let suppliers: FornecedorMotor[] = reqSuppliers.filter((sup) => !combination.fornecedores_bloqueados.includes(sup.id))
+    let suppliers: FornecedorMotor[] = reqSuppliers.filter(
+      (sup) => !combination.fornecedores_bloqueados.includes(sup.id)
+    )
 
     if (combination.preferencia_fornecedor_tipo === 'especifico') {
-      const { preferenceCategories, preferenceProducts } = preferencesResolver(combination)
+      const { preferenceCategories, preferenceProducts } =
+        preferencesResolver(combination)
       favoriteCategories.push(...preferenceCategories)
       favoriteProducts.push(...preferenceProducts)
-      suppliers = suppliers.filter((sup) => combination.fornecedores_especificos.includes(sup.id))
-    } else {
+      suppliers = suppliers.filter((sup) =>
+        combination.fornecedores_especificos.includes(sup.id)
+      )
+    } /* else {
       if (suppliers.length >= 5) {
         suppliers = suppliers.slice(0, 5)
       }
-    }
+    } */
 
     const reqMotor: MotorCombinacaoRequest = {
       suppliers,
@@ -69,13 +99,19 @@ export async function solveCombinations(prices: any[], products: ProdutoCesta[],
     const rawResultadoCotacao = await combinationSolverEngine(reqMotor)
     const resultadoCotacao = addSupplierNames(rawResultadoCotacao, prices)
 
-    solvedCombinations.push({ id: combination.id, nome: combination.nome, resultadoCotacao })
+    solvedCombinations.push({
+      id: combination.id,
+      nome: combination.nome,
+      resultadoCotacao
+    })
   }
 
   return solvedCombinations
 }
 
-async function combinationSolverEngine(req: MotorCombinacaoRequest): Promise<MotorCombinacaoResponse> {
+async function combinationSolverEngine(
+  req: MotorCombinacaoRequest
+): Promise<MotorCombinacaoResponse> {
   const result = await axios.post(`${process.env.API_MOTOR_COTACAO}/solve`, req)
   return result.data
 }
