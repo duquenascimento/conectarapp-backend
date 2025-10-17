e9639f756a6b7043d3bf92a52fa65e26492f44af
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-10-16 21:53:52 -0300

    feat: ajuste desconto conectar plus

--- src/utils/premium-suppliers.utils.ts
+++ src/utils/premium-suppliers.utils.ts
@@ -1,116 +1,105 @@
-import { DateTime } from 'luxon'
-import { type Supplier } from '../types/confirmTypes'
+import { DateTime } from 'luxon';
+import { get } from 'axios';
+import { type Supplier } from '../types/confirmTypes';
 import {
   type ProdutoCesta,
   type FornecedorMotor,
   type FornecedorPriceList,
   type MotorCombinacaoResponse,
   type MotorCombinacaoWithSupplierNames,
-  type Discount
-} from '../types/quotationTypes'
-import { ApiRepository } from '../repository/apiRepository'
-import { get } from 'axios'
+  type Discount,
+} from '../types/quotationTypes';
+import { ApiRepository } from '../repository/apiRepository';
+import { calculaPrecoBrutoConectarPlus } from './calculaPrecoBrutoConectarPlus';
 
-const apiDbConectar = new ApiRepository(process.env.API_DB_CONECTAR ?? '')
+const apiDbConectar = new ApiRepository(process.env.API_DB_CONECTAR ?? '');
 
 export async function getSuppliersFromPriceList(
   prices: any[],
-  cart: ProdutoCesta[]
+  cart: ProdutoCesta[],
 ): Promise<FornecedorMotor[] | null> {
-  const supplierList = prices.map(
-    (item: any) => item.supplier
-  ) as FornecedorPriceList[]
+  const supplierList = prices.map((item: any) => item.supplier) as FornecedorPriceList[];
 
-  const result = await fornecedoresCotacaoPremium(supplierList, cart)
+  const result = await fornecedoresCotacaoPremium(supplierList, cart);
 
-  return result
+  return result;
 }
 
 export async function fornecedoresCotacaoPremium(
   fornecedores: FornecedorPriceList[],
-  produtosCesta: ProdutoCesta[]
+  produtosCesta: ProdutoCesta[],
 ): Promise<FornecedorMotor[] | null> {
   if (fornecedores.length === 0) {
-    return null
+    return null;
   }
 
-  const fornecedoresCotacao: FornecedorMotor[] = []
+  const fornecedoresCotacao: FornecedorMotor[] = [];
   for (const item of fornecedores) {
     if (!isOpen(item) || item.minimumOrder > item.discount.orderValueFinish) {
-      continue
+      continue;
     }
     const produtosComPrecoFornecedor = produtosCesta.map((prodCesta) => {
-      const produto = item.discount.product.find((p) => p.sku === prodCesta.id)
-      return {
-        price: produto?.priceUnique,
-        productId: produto?.sku
-      }
-    })
+      const produto = item.discount.product.find((p) => p.sku === prodCesta.id);
 
-    const discounts = await getSupplierDiscountRange(item.externalId)
+      const precoProduto = calculaPrecoBrutoConectarPlus(
+        item.discount.discount,
+        Number(produto?.priceUnique),
+      );
+
+      return {
+        price: precoProduto,
+        productId: produto?.sku,
+      };
+    });
+
+    const discounts = await getSupplierDiscountRange(item.externalId);
 
     fornecedoresCotacao.push({
       id: item.externalId,
       products: produtosComPrecoFornecedor,
       discounts,
-      minValue: item.minimumOrder
-    })
+      minValue: item.minimumOrder,
+    });
   }
 
-  return fornecedoresCotacao
+  return fornecedoresCotacao;
 }
 
 export function addSupplierNames(
   motorResponse: MotorCombinacaoResponse,
-  supplierList: any[]
+  supplierList: any[],
 ): MotorCombinacaoWithSupplierNames {
   return {
     ...motorResponse,
     supplier: motorResponse.supplier.map((sup) => ({
       ...sup,
       name:
-        supplierList.find((s) => s.supplier.externalId === sup.id)?.supplier
-          .name ?? 'Nome não encontrado'
-    }))
-  }
+        supplierList.find((s) => s.supplier.externalId === sup.id)?.supplier.name ??
+        'Nome não encontrado',
+    })),
+  };
 }
 
 function isOpen(supplier: Supplier): boolean {
-  const currentDate = DateTime.now().setZone('America/Sao_Paulo')
+  const currentDate = DateTime.now().setZone('America/Sao_Paulo');
   const currentHour = Number(
-    `${
-      currentDate.hour.toString().length < 2
-        ? `0${currentDate.hour}`
-        : currentDate.hour
-    }${
-      currentDate.minute.toString().length < 2
-        ? `0${currentDate.minute}`
-        : currentDate.minute
-    }${
-      currentDate.second.toString().length < 2
-        ? `0${currentDate.second}`
-        : currentDate.second
-    }`
-  )
+    `${currentDate.hour.toString().length < 2 ? `0${currentDate.hour}` : currentDate.hour}${
+      currentDate.minute.toString().length < 2 ? `0${currentDate.minute}` : currentDate.minute
+    }${currentDate.second.toString().length < 2 ? `0${currentDate.second}` : currentDate.second}`,
+  );
 
-  return Number(supplier.hour.replaceAll(':', '')) > currentHour
+  return Number(supplier.hour.replaceAll(':', '')) > currentHour;
 }
 
-async function getSupplierDiscountRange(
-  externalId: string
-): Promise<Discount[]> {
+async function getSupplierDiscountRange(externalId: string): Promise<Discount[]> {
   try {
-    const discount = await apiDbConectar.callApi(
-      `/system/desconto/faixas/${externalId}`,
-      'GET'
-    )
+    const discount = await apiDbConectar.callApi(`/system/desconto/faixas/${externalId}`, 'GET');
 
-    return discount.data
+    return discount.data;
   } catch (error: any) {
     if (error.response?.status === 404) {
-      return []
-    } else {
-      throw error
+      return [];
     }
+    throw error;
   }
 }
