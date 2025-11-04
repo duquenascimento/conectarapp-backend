8f59f992e33ca5497728e76688ced2d830fd6d96
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-11-04 16:29:57 -0300

    feat: rota para delecao de usuarios/soft delete

--- src/route/userRoute.ts
+++ src/route/userRoute.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import * as jwt from 'jsonwebtoken';
-import { findUserById } from '../service/userService';
+import { findUserById, setUserAsInactive } from '../service/userService';
 import { UserResponse } from '../types/userResponseType';
 
 export const userRoute = async (server: FastifyInstance): Promise<any> => {
@@ -33,6 +33,41 @@ export const userRoute = async (server: FastifyInstance): Promise<any> => {
 
       return await res.status(200).send({
         data: userResponse,
+      });
+    } catch (err) {
+      const { message } = err as Error;
+      if (message === process.env.INTERNAL_ERROR_MSG) {
+        await res.status(500).send({
+          status: 500,
+          msg: message,
+        });
+      } else {
+        await res.status(401).send({
+          status: 401,
+          msg: message,
+        });
+      }
+    }
+  });
+
+  server.put('/delete-user', async (req, res): Promise<any> => {
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return await res.status(401).send({ message: 'Token não fornecido' });
+      }
+
+      const [, token] = authHeader.split(' ');
+      if (!token) {
+        return await res.status(401).send({ message: 'Formato do header inválido' });
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string };
+      const userId = decoded.id;
+
+      await setUserAsInactive(userId);
+      return await res.status(200).send({
+        message: 'Usuário excluido com sucesso!',
       });
     } catch (err) {
       const { message } = err as Error;
--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,14 +1,27 @@
-import { type user } from '@prisma/client'
-import { findById } from '../repository/userRepository'
-import { logRegister } from '../utils/logUtils'
+import { type user } from '@prisma/client';
+import { findById, softDeleteUser } from '../repository/userRepository';
+import { logRegister } from '../utils/logUtils';
 
 export const findUserById = async (id?: string): Promise<user> => {
   try {
-    const result = await findById(id ?? '')
-    if (result == null) throw new Error('not found', { cause: 'visibleError' })
-    return result
+    const result = await findById(id ?? '');
+    if (result == null) throw new Error('not found', { cause: 'visibleError' });
+    return result;
   } catch (err) {
-    if ((err as any).cause !== 'visibleError') await logRegister(err)
-    throw Error((err as Error).message)
+    if ((err as any).cause !== 'visibleError') await logRegister(err);
+    throw Error((err as Error).message);
   }
-}
+};
+
+export const setUserAsInactive = async (id: string): Promise<void> => {
+  try {
+    const user = await findById(id);
+    if (!user) {
+      throw new Error('not found', { cause: 'visibleError' });
+    }
+    await softDeleteUser(id);
+  } catch (err) {
+    if ((err as any).cause !== 'visibleError') await logRegister(err);
+    throw Error((err as Error).message);
+  }
+};
